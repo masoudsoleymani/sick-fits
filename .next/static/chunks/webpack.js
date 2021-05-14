f0c112495006065509aaf5e78f2386f7c7bb1912
Masoud Soleymani <masoud.soleymani@daimler.com>
2021-05-14 11:36:24 +0100

    Hook up File input and Form Styles

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b8ae0c43ab237fab8c40";
+/******/ 	var hotCurrentHash = "a087c46d7ace76c71228";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/sell.js
+++ pages/sell.js
@@ -1,3 +1,9 @@
+import CreateProduct from '../components/CreateProduct';
+
 export default function Sellpage() {
-  return <p>Sell-page...!</p>;
+  return (
+    <div>
+      <CreateProduct />
+    </div>
+  );
 }
